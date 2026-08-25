8e55bf4312b51e89500ab0f96b41dc5313450b8e
Jhamel Sumague <jhamelsumague70@gmail.com>
2026-08-25 15:01:46 +0800

    yes

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,9 @@
+import useState from "react";
+import type { ReactNode } from "react";
+
+import { CoupleSetupScreen } from "./src/screens/CoupleSetupScreen";
 import { InvitePartnerScreen } from "./src/screens/InvitePartnerScreen";
 
 export default function App() {
-  return <InvitePartnerScreen />;
+  return <CoupleSetupScreen />;
 }
